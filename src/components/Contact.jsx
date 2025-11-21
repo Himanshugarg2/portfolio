8f72aedd06dbd96b0651396b0fe03429d303db7a
Himanshu Garg <himanshu.garg@somaiya.edu>
2025-11-21 21:56:41 +0530

    Added code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,6 @@ export default function Contact() {
     <section className="text-center">
       <h2 className="text-2xl font-semibold mb-3">Contact</h2>
       <p>Email: himanshugarga.k@gmail.com</p>
-      <p>Phone: +91-9653232023</p>
     </section>
   );
 }
